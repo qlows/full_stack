eb23606202e1f2ad77426c16b639d116baeac355
qlows <79415956+qlows@users.noreply.github.com>
2022-11-17 14:16:34 -0500

    changes

--- lab10/lab11_101232721_axios_example/src/UserList.js
+++ lab10/lab11_101232721_axios_example/src/UserList.js
@@ -2,12 +2,24 @@ import React, { Component } from "react"
 
 export default class UserList extends Component {
 
-    
+    constructor(props){
+        super(props)
+        this.state = {
+            users: []
+        }
+    }
+
+
+
+    componentDidMount = () => {
+        this.getUsers()
+    }
 
     getUsers = () => {
         const USER_URL = "https://jsonplaceholder.typicode.com/users"
-        axios.get(USER_URL).then(res => {
-            console.log(res)
+        axios.get(USER_URL)
+        .then(res => {
+            console.log(res.data)
         })
         .catch(error => {
             console.log(error)
@@ -18,6 +30,11 @@ export default class UserList extends Component {
         return (
             <>
                 <h2>UserList</h2>
+                {
+                    this.state.users.map(user => (
+                        <p>{user.name} - {user.email}</p>
+                    ))
+                }
             </>
         )
     }

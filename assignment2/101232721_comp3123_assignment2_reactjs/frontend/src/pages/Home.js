2387baddcfcf2bb4b27ade4e6097dad048db9bed
qlows <79415956+qlows@users.noreply.github.com>
2022-11-30 17:20:17 -0500

    update gitignore

--- assignment2/101232721_comp3123_assignment2_reactjs/frontend/src/pages/Home.js
+++ assignment2/101232721_comp3123_assignment2_reactjs/frontend/src/pages/Home.js
@@ -1,7 +1,24 @@
 import React, {useState, useEffect} from 'react'
 import {Link} from "react-router-dom"
+import "./Home.css"
+import axios from "axios"
 
 const Home = () => {
+  const[data, setData] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, [])
+
+  const getUsers = async () => {
+    const res = await axios.get("http://localhost:3000/api/emp/employees");
+    if (res.status === 200){
+      setData(res.data);
+    }
+  }
+
+  console.log("data=>", data)
+
   return (
     <div>Home</div>
   )

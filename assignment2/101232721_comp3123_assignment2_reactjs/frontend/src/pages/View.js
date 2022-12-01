8492d3b1e59161e4e32b07e9ce26629efadb9138
qlows <79415956+qlows@users.noreply.github.com>
2022-12-01 16:03:27 -0500

    Updated View

--- assignment2/101232721_comp3123_assignment2_reactjs/frontend/src/pages/View.js
+++ assignment2/101232721_comp3123_assignment2_reactjs/frontend/src/pages/View.js
@@ -1,8 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams, Link } from "react-router-dom"
+import axios from 'axios'
+import "./View.css"
 
 const View = () => {
+    const [employee, setEmployee] = useState(null)
+    const { id } = useParams();
+
+    useEffect(() => {
+        if (id) {
+            getSingleEmp(id)
+        }
+    }, [id])
+
+    const getSingleEmp = async (id) => {
+        const res = await axios.get(`http://localhost:8080/api/emp/employees/${id}`)
+        if (res.status === 200) {
+            setEmployee({ ...res.data[0] });
+        }
+    }
+
     return (
-        <div>View</div>
+        <div style={{ marginTop: "150px" }}>
+            <div className='card'>
+                <div className='card-header'>
+                    <p>Employee Detail</p>
+                </div>
+                <div className='container'>
+                    <strong>ID: </strong>
+                    <strong>{employee.id}</strong>
+                    <br />
+                    <strong>First Name: </strong>
+                    <strong>{employee.first_name}</strong>
+                    <br />
+                    <strong>Last Name: </strong>
+                    <strong>{employee.last_name}</strong>
+                    <br />
+                    <strong>Email: </strong>
+                    <strong>{employee.email}</strong>
+                    <br />
+                    <strong>Gender: </strong>
+                    <strong>{employee.gender}</strong>
+                    <br />
+                    <strong>Salary: </strong>
+                    <strong>{employee.salary}</strong>
+                    <br />
+                    <Link to="/">
+                        <button className='btn btn-edit'>Back</button>
+                    </Link>
+                </div>
+            </div>
+        </div>
     )
 }
 

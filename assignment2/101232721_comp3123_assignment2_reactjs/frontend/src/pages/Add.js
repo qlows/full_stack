e75c3d0c346eb12459e7cc01ece94b6b0542b946
qlows <79415956+qlows@users.noreply.github.com>
2022-12-01 15:47:15 -0500

    Added employee update

--- assignment2/101232721_comp3123_assignment2_reactjs/frontend/src/pages/Add.js
+++ assignment2/101232721_comp3123_assignment2_reactjs/frontend/src/pages/Add.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { useNavigate, useLocation } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import "./Add.css"
 import axios from "axios"
 import { toast } from 'react-toastify';
@@ -18,27 +18,54 @@ const Add = () => {
 
   const nav = useNavigate();
 
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (id) {
+      getSingleEmp(id)
+    }
+  }, [id])
+
+  const getSingleEmp = async (id) => {
+    const res = await axios.get(`http://localhost:8080/api/emp/employees/${id}`)
+    if (res.status === 200) {
+      setState({ ...res.data[0] });
+    }
+  }
+
   const addEmployee = async (data) => {
     const res = await axios.post("http://localhost:8080/api/emp/employees", data);
     if (res.status === 200) {
       toast.success(res.data);
     }
-  }
-          // Validation not working
-             const handleSubmit = (e) => {
-              e.preventDefault();
-              if (!first_name || !last_name || !email || !gender || !salary) {
-                toast.error("Make sure all the fields are filled.")
-              } else {
-                addEmployee(state);
-                setTimeout(() => nav.push("/"), 500);
-              }
-            } 
-/*   const handleSubmit = (e) => {
+  };
+
+  const updateEmployee = async (data, id) => {
+    const res = await axios.put(`http://localhost:8080/api/emp/employees/${id}`, data);
+    if (res.status === 200) {
+      toast.success(res.data);
+    }
+  };
+
+  // Validation not working
+  const handleSubmit = (e) => {
     e.preventDefault();
-    addEmployee(state);
-    setTimeout(() => nav.push("/"), 500);
-  } */
+    if (!first_name || !last_name || !email || !gender || !salary) {
+      toast.error("Make sure all the fields are filled.")
+    } else {
+      if (!id) {
+        addEmployee(state);
+      } else {
+        updateEmployee(state, id)
+      }
+      setTimeout(() => nav.push("/"), 500);
+    }
+  };
+  /*   const handleSubmit = (e) => {
+      e.preventDefault();
+      addEmployee(state);
+      setTimeout(() => nav.push("/"), 500);
+    } */
 
   const handleInputChange = (e) => {
     let { first_name, defaultValue } = e.target;
@@ -95,7 +122,7 @@ const Add = () => {
           onChange={handleInputChange}
           defaultValue={salary}
         />
-        <input type="submit" defaultValue="Add"></input>
+        <input type="submit" defaultValue={id ? "Update" : "Add"}></input>
       </form>
     </div>
   )
